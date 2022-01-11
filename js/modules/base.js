66087998e4a7439d3dab08dc6b8991e6ba814cb6
mreichhoff <matthew.reichhoff@gmail.com>
2022-01-10 19:30:18 -0500

    Move graph operations to a separate module

--- js/modules/base.js
+++ js/modules/base.js
@@ -1,5 +1,6 @@
 import { faqTypes, showFaq } from "./faq.js";
 import { dataTypes, registerCallback, updateVisited, getVisited, addCards, getCardCount, inStudyList } from "./data-layer.js";
+import { addToGraph, initializeGraph, isInGraph } from "./graph.js";
 
 //TODO break this down further
 //refactor badly needed...hacks on top of hacks at this point
@@ -8,7 +9,6 @@ let currentExamples = {};
 let currentHanzi = null;
 let currentWord = null;
 let undoChain = [];
-let cy = null;
 let tabs = {
     explore: 'explore',
     study: 'study'
@@ -75,38 +75,6 @@ speechSynthesis.onvoiceschanged = function () {
     if (!zhTts) {
         zhTts = getZhTts();
     }
-};
-
-let levelColor = function (element) {
-    let level = element.data('level');
-    switch (level) {
-        case 6:
-            return '#68aaee';
-        case 5:
-            return '#de68ee';
-        case 4:
-            return '#6de200';
-        case 3:
-            return '#fff249';
-        case 2:
-            return '#ff9b35';
-        case 1:
-            return '#ff635f';
-    }
-};
-
-let layout = function (root, numNodes) {
-    //very scientifically chosen 95 (不 was slow to load)
-    //the grid layout appears to be far faster than cose
-    if (numNodes > 95) {
-        return {
-            name: 'grid'
-        };
-    }
-    return {
-        name: 'cose',
-        animate: false
-    };
 };
 
 let runTextToSpeech = function (text, anchors) {
@@ -315,106 +283,36 @@ let getCardFromDefinitions = function (text, definitionList) {
     result['en'] = answer;
     return result;
 };
-let setupCytoscape = function (root, elements, graphContainer) {
-    let prefersLight = window.matchMedia("(prefers-color-scheme: light)").matches;
-    cy = cytoscape({
-        container: graphContainer,
-        elements: elements,
-        layout: layout(root, elements.nodes.length),
-        style: [
-            {
-                selector: 'node',
-                style: {
-                    'background-color': levelColor,
-                    'label': 'data(id)',
-                    'color': 'black',
-                    'font-size': '16px',
-                    'text-valign': 'center',
-                    'text-halign': 'center'
-                }
-            },
-            {
-                selector: 'edge',
-                style: {
-                    'line-color': levelColor,
-                    'target-arrow-shape': 'none',
-                    'curve-style': 'straight',
-                    'label': 'data(words)',
-                    'color': (_ => prefersLight ? 'black' : '#eee'),
-                    'font-size': '10px',
-                    'text-background-color': (_ => prefersLight ? '#f9f9f9' : '#121212'),
-                    'text-background-opacity': '1',
-                    'text-background-shape': 'round-rectangle',
-                    'text-events': 'yes'
-                }
-            }
-        ],
-        maxZoom: 10,
-        minZoom: 0.5
-    });
-    cy.on('tap', 'node', function (evt) {
-        let id = evt.target.id();
-        let maxLevel = levelSelector.value;
-        updateUndoChain();
-        //not needed if currentHanzi contains id, which would mean the nodes have already been added
-        //includes O(N) but currentHanzi almost always < 10 elements
-        if (currentHanzi && !currentHanzi.includes(id)) {
-            addToExistingGraph(id, maxLevel);
-        }
-        setupExamples([id]);
-        persistState();
-        exploreTab.click();
-        mainHeader.scrollIntoView();
-        updateVisited([id]);
-    });
-    cy.on('tap', 'edge', function (evt) {
-        let words = evt.target.data('words');
-        updateUndoChain();
-        setupExamples(words);
-        persistState();
-        //TODO toggle functions
-        exploreTab.click();
-        mainHeader.scrollIntoView();
-        updateVisited([evt.target.source().id(), evt.target.target().id()]);
-    });
-};
 
-let addToExistingGraph = function (character, maxLevel) {
-    let result = { 'nodes': [], 'edges': [] };
-    let maxDepth = 1;
-    dfs(character, result, maxDepth, {}, maxLevel);
-    let preNodeCount = cy.nodes().length;
-    let preEdgeCount = cy.edges().length;
-    cy.add(result);
-    if (cy.nodes().length !== preNodeCount || cy.edges().length !== preEdgeCount) {
-        //if we've actually added to the graph, re-render it; else just let it be
-        cy.layout(layout(character, cy.nodes().length)).run();
+let nodeTapHandler = function (evt) {
+    let id = evt.target.id();
+    let maxLevel = levelSelector.value;
+    updateUndoChain();
+    //not needed if currentHanzi contains id, which would mean the nodes have already been added
+    //includes O(N) but currentHanzi almost always < 10 elements
+    if (currentHanzi && !currentHanzi.includes(id)) {
+        addToExistingGraph(id, maxLevel);
     }
+    setupExamples([id]);
+    persistState();
+    exploreTab.click();
+    mainHeader.scrollIntoView();
+    updateVisited([id]);
+};
+let edgeTapHandler = function (evt) {
+    let words = evt.target.data('words');
+    updateUndoChain();
+    setupExamples(words);
+    persistState();
+    //TODO toggle functions
+    exploreTab.click();
+    mainHeader.scrollIntoView();
+    updateVisited([evt.target.source().id(), evt.target.target().id()]);
+};
+let addToExistingGraph = function (character, maxLevel) {
+    addToGraph(character, maxLevel);
     //currentHanzi must be set up before this call
     currentHanzi.push(character);
-};
-
-let dfs = function (start, elements, maxDepth, visited, maxLevel) {
-    if (maxDepth < 0) {
-        return;
-    }
-    let curr = hanzi[start];
-    //todo does javascript have a set?
-    visited[start] = true;
-    for (const [key, value] of Object.entries(curr.edges)) {
-        //don't add outgoing edges when we won't process the next layer
-        if (maxDepth > 0 && value.level <= maxLevel) {
-            if (!visited[key]) {
-                elements.edges.push({ data: { id: Array.from(start + key).sort().toString(), source: start, target: key, level: value.level, words: value.words } });
-            }
-        }
-    }
-    elements.nodes.push({ data: { id: start, level: curr.node.level } });
-    for (const [key, value] of Object.entries(curr.edges)) {
-        if (!visited[key] && value.level <= maxLevel) {
-            dfs(key, elements, maxDepth - 1, visited, maxLevel);
-        }
-    }
 };
 let updateGraph = function (value, maxLevel) {
     document.getElementById('graph').remove();
@@ -424,10 +322,7 @@ let updateGraph = function (value, maxLevel) {
     mainContainer.append(nextGraph);
 
     if (value && hanzi[value]) {
-        let result = { 'nodes': [], 'edges': [] };
-        let maxDepth = 1;
-        dfs(value, result, maxDepth, {}, maxLevel);
-        setupCytoscape(value, result, nextGraph);
+        initializeGraph(value, maxLevel, nextGraph, nodeTapHandler, edgeTapHandler);
         currentHanzi = [value];
         persistState();
     }
@@ -607,7 +502,7 @@ recommendationsWorker.onmessage = function (e) {
         let usedRecommendation = false;
         for (let i = 0; i < e.data.recommendations.length; i++) {
             //don't bother recommending items already being shown in the graph
-            if (cy && cy.getElementById(e.data.recommendations[i]).length) {
+            if (isInGraph(e.data.recommendations[i])) {
                 continue;
             }
             recommendationMessage.removeAttribute('style');
