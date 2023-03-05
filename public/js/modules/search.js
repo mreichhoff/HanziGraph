285a2506ac67b31acea23f7746e8c3c875304340
mreichhoff <matthew.reichhoff@gmail.com>
2023-03-05 12:18:20 -0500

    Add initial multi-word english search capability

--- public/js/modules/search.js
+++ public/js/modules/search.js
@@ -232,10 +232,11 @@ function search(value, locale) {
         return;
     }
     if (value && looksLikeEnglish(value) && getActiveGraph().englishPath) {
+        value = value.toLowerCase();
         fetch(`/${getActiveGraph().englishPath}/${getPartition(value, getActiveGraph().partitionCount)}.json`)
             .then(response => response.json())
             .then(function (data) {
-                if (value !== hanziBox.value) {
+                if (value !== hanziBox.value.toLowerCase()) {
                     return false;
                 }
                 englishSearch(value, data[value]);
