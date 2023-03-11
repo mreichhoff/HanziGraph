f082b7d69c854228d2fde7b1a285a34e6bdb0a9e
mreichhoff <matthew.reichhoff@gmail.com>
2023-03-11 17:20:29 -0500

    Improve word header rendering
    No more ill-fitted separator underneath

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -25200,16 +25200,13 @@
         wordHolder.appendChild(wordSpan);
         addTextToSpeech(wordHolder, word, []);
         addSaveToListButton(wordHolder, word);
-        let separator = renderSeparator(wordHolder);
         if (active) {
             wordHolder.classList.add('active');
-            separator.classList.add('expand');
         }
         wordSpan.addEventListener('click', function () {
             if (!wordHolder.classList.contains('active')) {
                 document.querySelectorAll('.word-header').forEach(x => x.classList.remove('active'));
                 wordHolder.classList.add('active');
-                separator.classList.add('expand');
             }
             document.dispatchEvent(new CustomEvent('graph-update', { detail: word }));
         });
@@ -25388,7 +25385,7 @@
                 examplesList.append(item);
                 continue;
             }
-            renderExplore(words[i], item, definitionList, examples, numExamples, (!rendered && words.length > 1));
+            renderExplore(words[i], item, definitionList, examples, numExamples, !rendered);
             rendered = true;
 
             examplesList.append(item);
--- public/js/modules/explore.js
+++ public/js/modules/explore.js
@@ -205,16 +205,13 @@ let renderWordHeader = function (word, container, active) {
     wordHolder.appendChild(wordSpan);
     addTextToSpeech(wordHolder, word, []);
     addSaveToListButton(wordHolder, word);
-    let separator = renderSeparator(wordHolder);
     if (active) {
         wordHolder.classList.add('active');
-        separator.classList.add('expand');
     }
     wordSpan.addEventListener('click', function () {
         if (!wordHolder.classList.contains('active')) {
             document.querySelectorAll('.word-header').forEach(x => x.classList.remove('active'));
             wordHolder.classList.add('active');
-            separator.classList.add('expand');
         }
         document.dispatchEvent(new CustomEvent('graph-update', { detail: word }));
     });
@@ -393,7 +390,7 @@ let setupExamples = function (words, type, skipState) {
             examplesList.append(item);
             continue;
         }
-        renderExplore(words[i], item, definitionList, examples, numExamples, (!rendered && words.length > 1));
+        renderExplore(words[i], item, definitionList, examples, numExamples, !rendered);
         rendered = true;
 
         examplesList.append(item);
