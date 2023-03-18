4ed773c2211e040c3f17e4af7305f2f2b332c642
mreichhoff <matthew.reichhoff@gmail.com>
2023-03-18 14:03:51 -0400

    Update service worker caching logic

--- asset-service-worker.js
+++ asset-service-worker.js
@@ -1,15 +1,29 @@
-const cacheName = 'hanzigraph-1a2a800325d7b1bf4b66e89220c32110f0e301e2';
+const cacheName = 'hanzigraph-7073a1e632f65bc4d85e816455c4ddd7b2a86c03';
 self.addEventListener('fetch', (e) => {
     if (e.request.method === 'GET') {
         e.respondWith((async () => {
-            const r = await caches.match(e.request);
+            const cache = await caches.open(cacheName);
+            const r = await cache.match(e.request);
             if (r) {
                 return r;
             }
             const response = await fetch(e.request);
-            const cache = await caches.open(cacheName);
             cache.put(e.request, response.clone());
             return response;
         })());
     }
+});
+
+self.addEventListener("activate", (event) => {
+    event.waitUntil(
+        caches.keys().then((keyList) =>
+            Promise.all(
+                keyList.map((key) => {
+                    if (key !== cacheName) {
+                        return caches.delete(key);
+                    }
+                })
+            )
+        )
+    );
 });
