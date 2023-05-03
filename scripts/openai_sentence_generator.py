25b13c0c687de49a388effc9ed752be2b60cb379
mreichhoff <matthew.reichhoff@gmail.com>
2023-05-02 23:05:51 -0400

    Add chat support for sentence generation
    This adds to the completion API work. Next step: post processing

--- scripts/openai_sentence_generator.py
+++ scripts/openai_sentence_generator.py
@@ -27,26 +27,60 @@ def split(a, n):
 
 
 def generate_prompt(word):
-    return f"As a Chinese language instructor, give your students 3 memorable example sentences, with pinyin and an English translation, for the word ${word}. The format should be the Chinese sentence, then a comma, then the pinyin, then a comma, then the English translation, with no numbers in front."
+    return f"As a Chinese language instructor, give your students 3 memorable example sentences, with pinyin and an English translation, for the word ${word}. The format should be the Chinese sentence, then a newline, then the pinyin, then a newline, then the English translation, with no numbers in front."
+
+
+def generate_chat_instruction(word):
+    return f"Give your students 3 memorable example sentences, with pinyin and an English translation, for the word ${word}. The format should be the Chinese sentence, then a newline, then the pinyin, then a newline, then the English translation."
 
 
 BATCH_SIZE = 20
 
 
-def run_batch(batch):
+def run_batch_completions(batch):
     result = {}
     batched_words = list(batched(batch, BATCH_SIZE))
     for batch in batched_words:
         prompts = [generate_prompt(word) for word in batch]
-        response = openai.Completion.create(
-            model="text-davinci-003",
-            prompt=prompts,
-            temperature=0.7,
-            max_tokens=500,
-        )
+        try:
+            response = openai.Completion.create(
+                model="text-davinci-003",
+                prompt=prompts,
+                temperature=0.7,
+                max_tokens=500,
+            )
+        except:
+            # error calling? Just bail for now instead of trying to recover.
+            return result
         for choice in response.choices:
             result[batch[choice.index]] = choice.text
         # the limits are per minute. The requests take awhile, so could probably lower this
+        # could also use the response's initiated timestamp and do some math...
+        sleep(60)
+    return result
+
+
+def run_batch_chat(batch):
+    result = {}
+    # there's no batch endpoint for ChatCompletion, unfortunately.
+    # could do "give me sentences for the following wordlist", but that gets much more complicated.
+    for word in batch:
+        try:
+            # get the result; handle parsing and data wrangling as a post-process step.
+            # for whatever reason, the chat version does not obey formatting requests (maybe prompt issue, though several were tried)
+            result[word] = openai.ChatCompletion.create(
+                model="gpt-3.5-turbo",
+                messages=[
+                    {"role": "system", "content": "You are a Chinese language teacher."},
+                    {"role": "user",
+                        "content": generate_chat_instruction(word)}
+                ]
+            )
+        except:
+            # error calling? Just bail for now instead of trying to recover.
+            return result
+        # the limits are per minute. The requests take awhile, so could probably lower this
+        # could also use the response's initiated timestamp and do some math...
         sleep(60)
     return result
 
@@ -57,6 +91,8 @@ def main():
     parser.add_argument(
         '--word-list', help='the filename of a list of words to fetch examples for')
     parser.add_argument(
+        '--mode', help='completions or chat', default='chat')
+    parser.add_argument(
         '--num-threads', help='how many threads to call openai in parallel; their free tier: 3rpm', default=3)
     args = parser.parse_args()
     with open(args.word_list) as f:
@@ -66,7 +102,11 @@ def main():
         futures = []
         with concurrent.futures.ThreadPoolExecutor(max_workers=num_threads) as executor:
             for batch in batched_words:
-                futures.append(executor.submit(run_batch, batch))
+                if args.mode == 'completions':
+                    futures.append(executor.submit(
+                        run_batch_completions, batch))
+                else:
+                    futures.append(executor.submit(run_batch_chat, batch))
         all_results = {}
         for future in futures:
             all_results = all_results | future.result()
