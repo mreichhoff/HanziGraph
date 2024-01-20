d9589d69ab4e0f08c6d2042a4f8cf351c236ab43
mreichhoff <matthew.reichhoff@gmail.com>
2024-01-20 09:36:42 -0500

    Add measure word tags + render defs separately (#13)
    This commit improves definition rendering on the UI. It stops
    lumping all defs together in one, and adds a tag per measure
    word per definition. Upcoming changes will allow queries by
    measure word.
    
    TODO: the set of definitions included on page load has some
    undocumented dependencies on ordering and inclusion of all
    hanzi and components. I should probably fix that....

--- scripts/dictionary.py
+++ scripts/dictionary.py
@@ -11,6 +11,22 @@ def parse_cedict_line(line, character_type):
     return (simplified if character_type == 'simplified' else traditional, english, pinyin.rstrip().rstrip(']'))
 
 
+def parse_classifier(cedict_classifier, character_type):
+    # I...can I do this?
+    no_cl = cedict_classifier.split('CL:')[1]
+    classifiers_and_pinyin = no_cl.split(',')
+    # oh no
+    no_pinyin = [x.split('[')[0] for x in classifiers_and_pinyin]
+    result = []
+    for character in no_pinyin:
+        # oh not again
+        maybe_trad_and_simplified = character.split('|')
+        if len(maybe_trad_and_simplified) > 1:
+            result.append(maybe_trad_and_simplified[1] if character_type == 'simplified' else maybe_trad_and_simplified[0])
+        else:
+            result.append(maybe_trad_and_simplified[0])
+    return result
+
 # TODO should make this more generic...it's basically just a filter
 def get_allowlist(allowlist_filename):
     allowlist = set()
@@ -34,6 +50,9 @@ def get_dictionary_entries(dict_filename, character_type, filter_set):
                     if entry[0] not in result:
                         result[entry[0]] = []
                     for definition in entry[1]:
+                        if definition.startswith('CL:') and len(result[entry[0]]) > 0:
+                            result[entry[0]][len(result[entry[0]])-1]['measure'] = parse_classifier(definition, character_type)
+                            continue
                         if definition.startswith('variant of ') and entry[0] in definition:
                             continue
                         result[entry[0]].append(
