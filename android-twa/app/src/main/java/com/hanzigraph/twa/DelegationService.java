065401682debf33e39081fb3682983dc8a504a3b
mreichhoff <matthew.reichhoff@gmail.com>
2025-02-09 15:02:26 -0500

    Add initial Android TWA via bubblewrap (#62)
    This is an initial step towards making HanziGraph available in
    native app stores. Native versions remain planned, but as a v0
    a TWA seems interesting. Admittedly this adds ~nothing over PWA
    installation, just distribution benefits.

---
+++ android-twa/app/src/main/java/com/hanzigraph/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package com.hanzigraph.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
