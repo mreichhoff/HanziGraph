f130e8adf70d9f79df3319aa1def32f91057bd78
Matthew Reichhoff <matthew.reichhoff@gmail.com>
2025-02-11 19:40:38 -0500

    Add basic privacy info/account deletion per play store policy
    This change is quite minimal, as with other account related code.
    It is not intended to be polished, just allow deletion and
    communicate how data is used.

--- public/js/modules/auth.js
+++ public/js/modules/auth.js
@@ -1,70 +1,98 @@
 import { initialize } from "./firebase-init.js";
-import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, onAuthStateChanged, signInWithRedirect, GoogleAuthProvider, sendPasswordResetEmail } from "firebase/auth";
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, onAuthStateChanged, signInWithRedirect, GoogleAuthProvider, sendPasswordResetEmail, deleteUser } from "firebase/auth";
 
 initialize();
 let auth = getAuth();
-const googleProvider = new GoogleAuthProvider();
-onAuthStateChanged(auth, (user) => {
-    if (user) {
-        document.location.href = "/";
-    }
-});
-
-document.getElementById('signin-with-google').addEventListener('click', function () {
-    signInWithRedirect(auth, googleProvider);
-});
-
-document.getElementById('signin').addEventListener('submit', function (event) {
-    event.preventDefault();
-    event.stopPropagation();
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
-
-    const auth = getAuth();
-    signInWithEmailAndPassword(auth, email, password)
-        .then((_) => {
+if (document.location.href.includes('login')) {
+    const googleProvider = new GoogleAuthProvider();
+    onAuthStateChanged(auth, (user) => {
+        if (user) {
             document.location.href = "/";
-        })
-        .catch((_) => {
-            document.getElementById('error-message').style.visibility = 'visible';
-        });
-});
-document.getElementById('register').addEventListener('submit', function (event) {
-    event.preventDefault();
-    event.stopPropagation();
-    const auth = getAuth();
-    const email = document.getElementById('register-email').value;
-    const password = document.getElementById('register-password').value;
-    createUserWithEmailAndPassword(auth, email, password)
-        .then((_) => {
-            document.location.href = "/";
-        })
-        .catch((_) => {
-            document.getElementById('error-message').style.visibility = 'visible';
-        });
-});
-document.getElementById('register-link').addEventListener('click', function () {
-    document.getElementById('signin').style.display = 'none';
-    document.getElementById('login-header').innerText = "Register";
-    document.getElementById('register').removeAttribute('style');
-    document.getElementById('error-message').style.visibility = 'hidden';
-});
-document.getElementById('signin-link').addEventListener('click', function () {
-    document.getElementById('register').style.display = 'none';
-    document.getElementById('login-header').innerText = "Sign in";
-    document.getElementById('signin').removeAttribute('style');
-    document.getElementById('error-message').style.visibility = 'hidden';
-});
-document.getElementById('forgot-password').addEventListener('click', function () {
-    const auth = getAuth();
-    const email = document.getElementById('email').value;
-    if (!email) {
-        alert('Please enter your email address above. A recovery email will be sent to that address.');
-        return false;
-    }
-    sendPasswordResetEmail(auth, email)
-        .then(() => {
-            alert('A recovery email has been sent.');
-        });
+        }
+    });
 
-});
+    document.getElementById('signin-with-google').addEventListener('click', function () {
+        signInWithRedirect(auth, googleProvider);
+    });
+
+    document.getElementById('signin').addEventListener('submit', function (event) {
+        event.preventDefault();
+        event.stopPropagation();
+        const email = document.getElementById('email').value;
+        const password = document.getElementById('password').value;
+
+        const auth = getAuth();
+        signInWithEmailAndPassword(auth, email, password)
+            .then((_) => {
+                document.location.href = "/";
+            })
+            .catch((_) => {
+                document.getElementById('error-message').style.visibility = 'visible';
+            });
+    });
+    document.getElementById('register').addEventListener('submit', function (event) {
+        event.preventDefault();
+        event.stopPropagation();
+        const auth = getAuth();
+        const email = document.getElementById('register-email').value;
+        const password = document.getElementById('register-password').value;
+        createUserWithEmailAndPassword(auth, email, password)
+            .then((_) => {
+                document.location.href = "/";
+            })
+            .catch((_) => {
+                document.getElementById('error-message').style.visibility = 'visible';
+            });
+    });
+    document.getElementById('register-link').addEventListener('click', function () {
+        document.getElementById('signin').style.display = 'none';
+        document.getElementById('login-header').innerText = "Register";
+        document.getElementById('register').removeAttribute('style');
+        document.getElementById('error-message').style.visibility = 'hidden';
+    });
+    document.getElementById('signin-link').addEventListener('click', function () {
+        document.getElementById('register').style.display = 'none';
+        document.getElementById('login-header').innerText = "Sign in";
+        document.getElementById('signin').removeAttribute('style');
+        document.getElementById('error-message').style.visibility = 'hidden';
+    });
+    document.getElementById('forgot-password').addEventListener('click', function () {
+        const auth = getAuth();
+        const email = document.getElementById('email').value;
+        if (!email) {
+            alert('Please enter your email address above. A recovery email will be sent to that address.');
+            return false;
+        }
+        sendPasswordResetEmail(auth, email)
+            .then(() => {
+                alert('A recovery email has been sent.');
+            });
+
+    });
+}
+
+if (document.location.href.includes('privacy')) {
+    onAuthStateChanged(auth, (user) => {
+        if (user) {
+            deleteAccountButton.disabled = false;
+        } else {
+            deleteAccountButton.disabled = true;
+        }
+    });
+    const deleteAccountButton = document.getElementById('delete-account');
+    if (!getAuth() || !getAuth().currentUser) {
+        deleteAccountButton.disabled = true;
+    }
+    deleteAccountButton.addEventListener('click', function () {
+        const auth = getAuth();
+        if (auth && auth.currentUser && confirm(`Are you sure you want to delete your HanziGraph account, ${auth.currentUser.email}? This cannot be undone.`)) {
+            const user = auth.currentUser;
+            deleteUser(user).then(() => {
+                alert('Your account has been deleted.');
+                document.location.href = "/";
+            }).catch((error) => {
+                alert(`Error: ${error}. You may need to sign in first, to avoid fraudulent deletions. Email hanzigraph@googlegroups.com for support.`);
+            });
+        }
+    });
+}
