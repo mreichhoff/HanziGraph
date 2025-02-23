df09331d08f610b05d8825fc1aabf0ff499ed85e
Matthew Reichhoff <matthew.reichhoff@gmail.com>
2025-02-23 11:48:37 -0500

    Add per-user per-day quota for AI functions

--- functions/src/auth.ts
+++ functions/src/auth.ts
@@ -2,6 +2,24 @@ import { ActionContext } from "genkit";
 import { getFirestore } from "firebase-admin/firestore";
 import * as logger from "firebase-functions/logger";
 
+const DEFAULT_DAILY_LIMIT = 100;
+
+function getISODate(date: Date) {
+    function pad(number: number) {
+        if (number < 10) {
+            return '0' + number;
+        }
+        return number;
+    }
+
+    return (
+        date.getFullYear() +
+        '-' +
+        pad(date.getMonth() + 1) +
+        '-' +
+        pad(date.getDate()));
+};
+
 export async function isUserAuthorized(context: ActionContext | undefined): Promise<boolean> {
     if (!context || !context.auth?.uid) {
         return false;
@@ -9,9 +27,46 @@ export async function isUserAuthorized(context: ActionContext | undefined): Prom
     const userId = context.auth.uid;
 
     const firestore = getFirestore();
-    let docRef = firestore.doc(`permissions/${userId}`);
-    const documentSnapshot = await docRef.get();
-    if (!documentSnapshot.exists || documentSnapshot.get('ai') !== true) {
+    const docRef = firestore.doc(`permissions/${userId}`);
+    try {
+        // an extremely similar use case is shown on:
+        // https://firebase.google.com/docs/firestore/manage-data/transactions#passing_information_out_of_transactions
+        // we use a transaction to ensure the same document isn't written concurrently without our awareness
+        // and because of that, our increment can be trusted
+        // and if the user exceeds a quota the admin can set (which the user cannot write, security rules enforce it),
+        // they get rejected. It's true that the client-side Firebase Vertex API has per-user-per-minute limits,
+        // but using this approach instead allows:
+        // a) not relying on client-side appcheck
+        // b) authorizing individual users (though I may set it up to be default authorized, or easy to obtain)
+        // c) setting per-user limits, with more flexibility than just "N per minute per user"
+        //    e.g., an admin user could have a high limit, a new user could have a low one, and it's per-day vs
+        //    per-minute (so more able to handle a quick few queries during a moment of intensive use)
+        // d) this also banks on the notion of backend integration having more flexibility long-term (e.g.,
+        //    to use non-gemini models or entirely removing the google APIs).
+        await firestore.runTransaction(async transaction => {
+            const date = new Date();
+            // note that this approach does assume we wouldn't get systematic clock skew
+            // that just keeps moving into the past with every request or something, but
+            // that seems a reasonable assumption.
+            const isoDate: string = getISODate(date);
+            const documentSnapshot = await transaction.get(docRef);
+            if (!documentSnapshot.exists || documentSnapshot.get('ai') !== true) {
+                logger.info(`user ${userId} cannot use AI features`);
+                throw 'User unauthorized';
+            }
+            const dailyLimit: number = documentSnapshot.get('dailyLimit') || DEFAULT_DAILY_LIMIT;
+            const useToday = documentSnapshot.get(isoDate) || 0;
+            const updatedTotal = useToday + 1;
+            if (updatedTotal > dailyLimit) {
+                logger.info(`user ${userId} out of quota for ${isoDate}`);
+                throw 'User exceeded daily limit';
+            }
+            // note that this does allow the document to grow unbounded with new dates.
+            // however, the max number of keys is reportedly 20k, so we should have ~50 years
+            // of every-day use.
+            transaction.update(docRef, isoDate, updatedTotal);
+        });
+    } catch (ex) {
         return false;
     }
     // user ID is a random string...shouldn't be sensitive for logging (as opposed to email address)
